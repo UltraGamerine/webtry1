53942a2c7e462a53a2bc49409bfad51859e5f728
Aryan Vats <64453040+UltraGamerine@users.noreply.github.com>
2023-03-06 22:53:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,28 @@
 function openNav() {
       document.getElementById("mySidebar").style.width = "250px";
       document.getElementById("main").style.marginLeft = "250px";
+
+
     }
     
-    /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
     function closeNav() {
       document.getElementById("mySidebar").style.width = "0";
       document.getElementById("main").style.marginLeft = "0";
     }
-    
-    
-    
+  
+/*  
+let sideState=0;
+function openNav(){
+  if (sideState===0){
+      document.getElementById("mySidebar").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+      sideState=1;
+  } 
+else{
+      document.getElementById("main").style.marginLeft = 0;
+      document.getElementById("mySidebar").style.width = 0;
+      sideState=0;
+
+  }
+}
+*/
